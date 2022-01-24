61477562178988cc00f4089feb3cf5b4a8cdb781
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-01-24 22:15:39 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+(function(){
+    document.querySelector("body").innerHTML="hello";
+    let i=0;
+    let st="abcde";
+    for (let s = 0; s < st.length; s++) {
+        const element = st[s];
+        console.log(element);
+        
+        
+    }
+})();
